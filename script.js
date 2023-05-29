0ea53fd2f902305cbb31ca4a994e3ccb5fa31236
Vidhi Gaydhane <120549998+VIDHI-tech@users.noreply.github.com>
2023-05-29 08:16:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function delay() {
+	return new Promise((resolve, reject) =>{
+		setTimeOut(() => {
+			resolve("Hello, world!");
+		}, 1000);
+	});
+}
+ 
+const outputElement = document.getElementById("output");
+
+delay().then((result) => {
+	outputElement.innerText = result;
+});
